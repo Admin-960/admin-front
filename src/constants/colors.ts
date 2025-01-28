54782e62e735f9bcbf157092f4a4395e2c18467e
Ivan <ivanvolodin960@gmail.com>
2025-01-28 22:02:42 +0300

    AF-12: add colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,5 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+export const accentColor = '#E30B13'
+export const bgColor = '#191B1F'
+
 export const COLORS = {
-	primary: '#EF4444',
-	bg: '#191B28',
+	primary: '#E30B13',
+	black: colors.black,
+	white: colors.white,
+	gray: {
+		300: '#d9dae8',
+		500: '#999AA5',
+		600: '#66676E',
+		700: '#39393f',
+		800: '#242529',
+		900: '#191B1F',
+		950: '#101215'
+	},
+	transparent: colors.transparent,
+	yellow: {
+		700: '#F5C521'
+	},
 	border: 'rgba(255, 255, 255, 0.07)'
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,55 @@ export default {
 	theme: {
 		extend: {
 			colors: COLORS,
+
 			padding: {
 				layout: '1.4rem'
+			},
+
+			spacing: {
+				0.5: '0.12rem',
+				layout: '2.75rem'
+			},
+			fontSize: {
+				'2lg': '1.38rem'
+			},
+			borderRadius: {
+				image: '0.5rem',
+				layout: '0.8rem'
 			},
 			transitionTimingFunction: {
 				DEFAULT: 'ease-in-out'
 			},
 			transitionDuration: {
-				DEFAULT: '333ms'
+				DEFAULT: '200ms'
+			},
+			keyframes: {
+				fade: {
+					from: { opacity: '0' },
+					to: { opacity: '1' }
+				},
+				scaleIn: {
+					'0%': {
+						opacity: '0',
+						transform: 'scale(0.9)'
+					},
+					'50%': {
+						opacity: '0.3'
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'scale(1)'
+					}
+				}
+			},
+			animation: {
+				fade: 'fade .5s ease-in-out',
+				scaleIn: 'scaleIn .35s ease-in-out'
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3'
 			}
 		}
 	},
